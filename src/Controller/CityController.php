3bedcf7254e273da91167b2ecac510839e3b8b07
novaedra <leofouquier20111999@gmail.com>
2020-01-07 16:54:14 +0100

    more security and check for city

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App\Controller;
+
 use App\Repository\CityRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * @package App\Controller
  *
@@ -14,35 +17,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class CityController
 {
     private $cityRepository;
+
     public function __construct(CityRepository $cityRepository)
     {
         $this->cityRepository = $cityRepository;
     }
+
     /**
      * @Route("/add", name="add_city", methods={"POST"})
      * @param Request $request
      * @return JsonResponse
      */
-    public function addCity(Request $request) : JsonResponse
+    public function addCity(Request $request): JsonResponse
     {
-        $data = json_decode($request->getContent(),true);
+        $data = json_decode($request->getContent(), true);
         $name = $data['name'];
         $county_code = $data['countyCode'];
         $region = $data['region'];
         $mail_city = $data['mailCity'];
-        if(empty($name) || empty($county_code) || empty($region) || empty($mail_city))
-        {
+        if (empty($name) || empty($county_code) || empty($region) || empty($mail_city)) {
             throw new NotFoundHttpException('Expecting mandatory parameters!');
         }
-        $this->cityRepository->saveCity($name,$county_code,$region,$mail_city);
+        $this->cityRepository->saveCity($name, $county_code, $region, $mail_city);
         return new JsonResponse(['status' => 'City add !'], Response::HTTP_CREATED);
     }
+
     /**
      * @Route("/get/{id}", name="get_one_city", methods={"GET"})
      */
     public function getOneCity($id)
     {
-        $city = $this->cityRepository->findOneBy(['id' => $id ]);
+        $city = $this->cityRepository->findOneBy(['id' => $id]);
         $data = [
             'id' => $city->getId(),
             'name' => $city->getName(),
@@ -54,13 +59,14 @@ class CityController
         ];
         return new JsonResponse(['city' => $data], Response::HTTP_OK);
     }
+
     /**
      * @Route("/all", name="get_all_city", methods={"GET"})
      */
     public function getAllCity(): JsonResponse
     {
         $citys = $this->cityRepository->findAll();
-        foreach ($citys as $city){
+        foreach ($citys as $city) {
             $data[] = [
                 'id' => $city->getId(),
                 'name' => $city->getName(),
@@ -73,16 +79,18 @@ class CityController
         }
         return new JsonResponse(['citys' => $data], Response::HTTP_OK);
     }
+
     /**
      * @Route("/update/{id}", name="update_city", methods={"PUT"})
      */
-    public function updateCity($id,Request $request): JsonResponse
+    public function updateCity($id, Request $request): JsonResponse
     {
         $city = $this->cityRepository->findOneBy(['id' => $id]);
-        $data = json_decode($request->getContent(),true);
+        $data = json_decode($request->getContent(), true);
         $this->cityRepository->updateCity($city, $data);
         return new JsonResponse(['status' => 'city updated!']);
     }
+
     /**
      * @Route("/delete/{id}", name="delete_city", methods={"DELETE"})
      */
--- src/Repository/CityRepository.php
+++ src/Repository/CityRepository.php
@@ -28,26 +28,32 @@ class CityRepository extends ServiceEntityRepository
     {
         $newCity = new City();
 
-        $newCity->setName($name)
-            ->setCountyCode($county_code)
-            ->setRegion($region)
-            ->setMailCity($mail_city);
+        //vérification intégrité et validité des données
+        empty($name) ? true : is_string($name) ? true : strlen($name) > 100 ? true : $newCity->setName($name);
+        empty($county_code) ? true : is_string($county_code) ? true : strlen($county_code) > 10 ? true : $newCity->setName($county_code);
+        empty($region) ? true : is_string($region) ? true : strlen($region) > 100 ? true : $newCity->setName($region);
+        empty($mail_city) ? true : is_string($mail_city) ? true : strlen($mail_city) > 150 ? true : $newCity->setName($mail_city);
+
         $this->manager->persist($newCity);
         $this->manager->flush();
     }
 
     public function updateCity(City $city, $data)
     {
-        empty($data['name']) ? true : $city->setName($data['name']);
-        empty($data['countyCode']) ? true : $city->setName($data['countyCode']);
-        empty($data['region']) ? true : $city->setName($data['region']);
-        empty($data['mailCity']) ? true : $city->setName($data['mailCity']);
+        //vérification intégrité et validité des données
+        empty($data['name']) ? true : is_string($data['name']) ? true : strlen($data['name']) > 100 ? true : $city->setName($data['name']);
+        empty($data['countyCode']) ? true : is_string($data['countyCode']) ? true : strlen($data['countyCode']) > 10 ? true : $city->setCountyCode($data['countyCode']);
+        empty($data['region']) ? true : is_string($data['region']) ? true : strlen($data['region']) > 100 ? true : $city->setRegion($data['region']);
+        empty($data['mailCity']) ? true : is_string($data['mailCity']) ? true : strlen($data['mailCity']) > 150 ? true : $city->setMailCity($data['mailCity']);
+        $city->setUpdatedAt(new \Datetime("now"));
 
         $this->manager->flush();
     }
 
     public function removeCity(City $city)
     {
+        //auth admin stp
+
         $this->manager->remove($city);
         $this->manager->flush();
     }
