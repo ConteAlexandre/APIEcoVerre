6034453fa223180e5a682f653ffb10b57da46e6f
novaedra <leofouquier20111999@gmail.com>
2020-01-15 11:54:01 +0100

    fix city name and country code in list api

--- src/Controller/GlassDumpController.php
+++ src/Controller/GlassDumpController.php
@@ -33,6 +33,11 @@ class GlassDumpController
     public function addGlassDump(Request $request)
     {
         $data = json_decode($request->getContent(), true);
+
+        if (empty($data['numBorn']) || empty($data['volume']) || empty($data['landMark']) || empty($data['collectDay']) || empty($data['coordinates']) || empty($data['nameCity']) || empty($data['countryCode'])) {
+            return new JsonResponse('Missing parameter - please try again');
+        }
+
         $numBorn = $data['numBorn'];
         $volume = $data['volume'];
         $landMark = $data['landMark'];
@@ -41,13 +46,7 @@ class GlassDumpController
         $nameCity = $data['nameCity'];
         $countryCode = $data['countryCode'];
 
-        $dump = $this->glassDumpRepository->findOneBy(['number_borne' => $numBorn]);
-
-        if (empty($data['numBorn']) || empty($data['volume']) || empty($data['landMark']) || empty($data['collectDay']) || empty($data['coordinates']) || empty($data['damage']) || empty($data['isFull']) || empty($data['isEnable']) || empty($data['nameCity']) || empty($data['countryCode'])) {
-            return new JsonResponse('Missing parameter - please try again');
-        }
-
-        $dump = $this->glassDumpRepository->findOneBy(['number_borne' => $numBorn]);
+        $dump = $this->glassDumpRepository->findBy(['coordonate' => "POINT($coordonate)"]);
 
         if (empty($dump)) {
             $this->glassDumpRepository->saveDump($numBorn, $volume, $landMark, $collectDay, $coordonate, $nameCity, $countryCode);
@@ -108,7 +107,8 @@ class GlassDumpController
                     'damage' => $dump->getDammage(),
                     'isFull' => $dump->getIsFull(),
                     'isEnable' => $dump->getIsEnable(),
-                    'idCity' => $dump->getId(),
+                    'city_name' => $dump->getCityName(),
+                    'country_code' => $dump->getCountryCode(),
                     'createdAt' => $dump->getCreatedAt(),
                     'updatedAt' => $dump->getUpdatedAt(),
                 ];
