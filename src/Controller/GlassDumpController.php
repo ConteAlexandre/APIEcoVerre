11a6a13654c34848282584d156f4afff52dc1b7a
ConteAlexandre <theluffy@hotmail.fr>
2020-01-15 17:02:33 +0100

    Add package swagger for doc

--- src/Controller/GlassDumpController.php
+++ src/Controller/GlassDumpController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+
 
 /**
  * @package App\Controller
